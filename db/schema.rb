e125cb71b3bf83e04dc3d711fa1216ac925fcd16
axiner <axiner.w@gmail.com>
2016-04-06 15:29:30 -0400

    final commit

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160406173730) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "pictures", force: :cascade do |t|
     t.string   "artist"
     t.string   "title"
