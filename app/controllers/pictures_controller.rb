8b49746c80c73b03bcd2385d1aa1dff1f4914286
axiner <axiner.w@gmail.com>
2016-04-06 14:27:30 -0400

    Adding feature to edit pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,25 +1,25 @@
 class PicturesController < ApplicationController
 
-  def index
-    @pictures = Picture.all
-  end
-
-  def show
-    @picture = Picture.find(params[:id])
-  end
-
-  def new
-    @picture = Picture.new
-  end
-
-  def create
-    @picture = Picture.new(picture_params)
-    if @picture.save
-      redirect_to pictures_url
-    else
-      render :new
+    def index
+      @pictures = Picture.all
     end
-  end
+
+    def show
+      @picture = Picture.find(params[:id])
+    end
+
+    def new
+      @picture = Picture.new
+    end
+
+    def create
+      @picture = Picture.new(picture_params)
+      if @picture.save
+        redirect_to pictures_url
+      else
+        render :new
+      end
+    end
 
     private
     def pictures_params
